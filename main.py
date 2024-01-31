acf3a02394dd15478a5da7b82eefcdddf533c441
jorgelpat <156463233+jorgelpat@users.noreply.github.com>
2024-01-31 11:38:31 -0500

    a

--- main.py
+++ main.py
@@ -1,27 +1,23 @@
-#from module.camper import *
+# from module.camper import *
+# from module.camper import save
+# from module.camper import delete as eliminar
 import json
 from os import system
-import module.camper as camper #importa el modulo (import nombreDeCarpeta.nombreArchivo)
+import module.camper as camper
 import module.trainer as trainer
-import module.validate as validate
 from module.validate import menuNoValid
-#from module.data import camper as dataCamper
-#from module.camper import * #Con (*) importan todos,
-#from module.camper import save, delete  
-#from module.trainer import save, delete
-
 def menu():
-    print("Sistema de almacenamiento de datos")
-    print("\t1.Informacion del camper")
-    print("\t2.Informacion del trainer")
+    print("Sistema de almacenamiento de datos para campus")
+    print("\t1. Informacion del camper")
+    print("\t2. Informacion del trainer")
     print("\t0. Salir")
-
-bandera = True
-while(bandera):
+bandera=True
+while (bandera):
     menu()
     opc = int(input())
     match(opc):
         case 1:
+            
             with open("module/storage/camper.json", "r") as f:
                 camper.camper = json.loads(f.read())
                 f.close()
